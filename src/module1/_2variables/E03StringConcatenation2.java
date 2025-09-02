9c314af55cccf98636812c8c248e10ee4eebfa04
tmantineo <e.l.mantineo@wustl.edu>
2025-09-01 20:36:32 -0500

    Done

--- src/module1/_2variables/E03StringConcatenation2.java
+++ src/module1/_2variables/E03StringConcatenation2.java
@@ -4,6 +4,7 @@ public class E03StringConcatenation2 {
 	//Exercise 3: Add a print statement that concatenates
 	//"Favorite color is " with the given variable
 	public static void main(String[] args) {
-		String color = "red";
+		String color = "blue";
+		System.out.println("My favorite color is " + color);
 	}
 }
